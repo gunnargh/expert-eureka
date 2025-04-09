12ef387fefbc11236bfb795696bdec11680f3e6b
Gunnar Geir Helgason <gunnargeir89@gmail.com>
2025-04-09 20:13:10 +0000

    Update dart.dart

--- dart/bin/dart.dart
+++ dart/bin/dart.dart
@@ -3,4 +3,5 @@ import 'package:dart/dart.dart' as dart;
 void main(List<String> arguments) {
   print('Hello world: ${dart.calculate()}!');
   print("I am so bad, I code in chrome");
+  print("What is this?");
 }
